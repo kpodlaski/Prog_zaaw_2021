f1c4f68f47b7da8ee76d42f83b26786323bb9218
kpodlaski <podlaski@uni.lodz.pl>
2021-04-14 15:05:20 +0200

    Zajecia 14.04.2021 v2

--- nbp.py
+++ nbp.py
@@ -1,16 +1,23 @@
 import json
 import urllib.request as req
 import matplotlib.pyplot as plt
+from calendar import monthrange
 
 # install in the system
 # pip matplotlib
 
-adres = "http://api.nbp.pl/api/exchangerates/rates/A/EUR/2021-03-01/2021-03-31?format=json"
-# source = req.urlopen(adres)
-# data = source.read().decode()
-# source.close()
-with req.urlopen(adres) as url:
-    data = json.loads(url.read().decode())
+def nbp_currency(cur,month, year):
+    week, lastday = monthrange(year, int(month))
+    adres = "http://api.nbp.pl/api/exchangerates/rates/A/{0}/{1}-{2}-01/{1}-{2}-{3}?format=json".format(cur,year,month,lastday)
+    # source = req.urlopen(adres)
+    # data = source.read().decode()
+    # source.close()
+    with req.urlopen(adres) as url:
+        data = json.loads(url.read().decode())
+    return data
+
+currency = "gbp"
+data  = nbp_currency(currency, '03', 2021)
 
 print(data['currency'])
 print(data['rates'])
@@ -26,8 +33,8 @@ print(mids)
 
 plt.plot(days,mids, label="Trend")
 plt.scatter(days,mids, c='red', label="Dane rzeczywiste")
-plt.xlabel("Dni miesiąca")
-plt.ylabel("Kurs EURO")
+plt.xlabel("Dni miesiąca ")
+plt.ylabel("Kurs "+currency)
 plt.legend()
 plt.savefig("out/kurs_euro.png")
 plt.show()
